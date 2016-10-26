a13657989c6e501e183cae06b64b9759972b0c22
Sergio Blazquez <sergiobl88@gmail.com>
2016-10-26 01:01:03 +0100

    Inventario_P1 - Añadir cargador de base de datos de objetos

---
+++ Assets/Scripts/Inventario/BaseDatosHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class BaseDatosHandler : MonoBehaviour {
+
+    public BaseDatosObjeto bd;
+
+	// Use this for initialization
+	void Start () {
+        string datos = File.ReadAllText(Application.dataPath + "/Resources/objetos.json");
+        bd = JsonUtility.FromJson<BaseDatosObjeto>(datos);
+	}
+
+    public Objeto buscarObjetoPorId(int id)
+    {
+        return bd.baseDatos.Find(objeto => objeto.id == id);
+    }
+	
+}
