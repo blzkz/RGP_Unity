99d6c3de73ceefcf78a0f5d6b437fa69a530cf6b
Sergio Blazquez <sergiobl88@gmail.com>
2016-11-21 01:33:46 +0000

    Inventario_P6 - Creacion de la interfaz de usuario de inventario

---
+++ Assets/Scripts/Inventario/UI/cargaObjetos.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class cargaObjetos : MonoBehaviour {
+
+    public GameObject objeto;
+    private Inventario inventario;
+    private BaseDatosHandler handler;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("start");
+        inventario = Inventario.inv;
+        handler = GameManager.instancia.GetComponent<BaseDatosHandler>();
+        this.pintaObjetos();
+	}
+	
+    void pintaObjetos()
+    {
+        foreach (instanciaObjeto obj in inventario.listaObjetos)
+        {
+            GameObject instanciaObjeto = Instantiate(objeto);
+            Objeto detalleObjeto = handler.buscarObjetoPorId(obj.id);
+            instanciaObjeto.transform.GetComponentInChildren<Text>().text = detalleObjeto.nombre;
+            instanciaObjeto.transform.SetParent(this.transform);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
