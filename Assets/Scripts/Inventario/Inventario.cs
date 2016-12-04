fa0bc7faeef2afb60c62e671e96d9902431a7fc7
Sergio Blazquez <sergiobl88@gmail.com>
2016-12-04 17:05:44 +0000

    Inventario_P6-2 añadida funcionalidad de soltar objeto

--- Assets/Scripts/Inventario/Inventario.cs
+++ Assets/Scripts/Inventario/Inventario.cs
@@ -2,13 +2,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System;
 
 public class Inventario : MonoBehaviour {
 
     public static Inventario inv = null;
     public List<instanciaObjeto> listaObjetos;
+
+    internal void borraObjetoPorPosicion(int posicion)
+    {
+        if (posicion < listaObjetos.Count)
+        {
+            listaObjetos.Remove(listaObjetos[posicion]);
+        }
+    }
+
     public int maxNumObj = 5;
     public BaseDatosHandler handler = null;
+    public GameObject menuObjeto;
 
     void Awake()
     {
@@ -97,6 +108,24 @@ public class Inventario : MonoBehaviour {
 
     public bool canAdd(int id) {
         return this.listaObjetos.Count < this.maxNumObj;
+    }
+
+    public void activarMenuObjeto(int id, int posicion, RectTransform rectT, Vector2 eventPos, GameObject objetoUI)
+    {
+        if (menuObjeto == null)
+        {
+            menuObjeto = GameManager.instancia.inventarioUI.transform.GetChild(0).Find("MenuObjeto").gameObject;
+        }
+        menuObjetoInfo menu = menuObjeto.GetComponent<menuObjetoInfo>();
+        menu.id = id;
+        menu.posicion = posicion;
+        menu.objetoUI = objetoUI;
+        Rect rec = RectTransformUtility.PixelAdjustRect(rectT, GameManager.instancia.inventarioUI.GetComponent<Canvas>());
+        float offsetX = this.menuObjeto.GetComponent<RectTransform>().rect.width / 2;
+        float offsetY = this.menuObjeto.GetComponent<RectTransform>().rect.height / 2;
+        Vector3 post = new Vector3(eventPos.x + offsetX, eventPos.y - offsetY);
+        this.menuObjeto.GetComponent<RectTransform>().position = post;
+        this.menuObjeto.SetActive(true);
     }
 }
 
---
+++ Assets/Scripts/Inventario/UI/menuObjetoInfo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuObjetoInfo : MonoBehaviour {
+
+    public int id;
+    public int posicion;
+    public GameObject objetoUI;
+
+    public void soltarObjeto()
+    {
+        Inventario.inv.borraObjetoPorPosicion(posicion);
+        Destroy(objetoUI);
+        this.gameObject.SetActive(false);
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
